3008f31bdd29fcb50bef280ecee0b0100ffee9d4
danisharora099 <danisharora099@gmail.com>
2024-04-07 13:59:52 +0300

    chore(rln-js): convert to typescript

--- examples/rln-js/src/waku.ts
+++ examples/rln-js/src/waku.ts
@@ -1,4 +1,4 @@
-import { createLightNode, waitForRemotePeer } from "@waku/sdk";
+import {type LightNode, createLightNode, waitForRemotePeer, type IFilterSubscription, IDecodedMessage } from "@waku/sdk";
 
 import {
   ProtoChatMessage,
@@ -7,11 +7,24 @@ import {
   MEMBERSHIP_HASH,
   MEMBERSHIP_PASSWORD,
 } from "./const";
+import { RLNDecoder, RLNEncoder, RLNInstance } from "@waku/rln";
+import { StatusChangeArgs } from "./types";
 
-export async function initWaku({ rln, onStatusChange }) {
-  let node;
-  let encoder, decoder;
-  let subscription;
+type Args = {
+  rln: RLNInstance,
+  onStatusChange: (args: StatusChangeArgs) => void,
+};
+
+type IChatMessage = {
+  nick: string,
+  text: string,
+  timestamp: number,
+};
+
+export async function initWaku({ rln, onStatusChange }: Args) {
+  let node: LightNode;
+  let encoder: RLNEncoder, decoder: RLNDecoder<IDecodedMessage>;
+  let subscription: IFilterSubscription;
 
   const onInitWaku = async () => {
     encoder = await rln.createEncoder({
@@ -25,16 +38,20 @@ export async function initWaku({ rln, onStatusChange }) {
     });
     decoder = rln.createDecoder(CONTENT_TOPIC);
   
-    onStatusChange("Initializing Waku...");
+    onStatusChange({
+      newStatus: "Initializing Waku..."
+    });
     node = await createLightNode({
       defaultBootstrap: true,
     });
-    onStatusChange("Waiting for peers");
+    onStatusChange({
+      newStatus: "Waiting for peers"
+    });
     await node.start();
     await waitForRemotePeer(node);
   };
 
-  const onSend = async (nick, text) => {
+  const onSend = async (nick: string, text: string) => {
     const timestamp = new Date();
     const msg = ProtoChatMessage.create({
       text,
@@ -48,21 +65,28 @@ export async function initWaku({ rln, onStatusChange }) {
     console.log("Message sent:", res);
   };
 
-  const onSubscribe = async (cb) => {
-    onStatusChange("Subscribing to content topic...");
+  const onSubscribe = async (cb: (nick: string, text: string, timestamp: number, proofStatus: string) => void ) => {
+    onStatusChange({
+      newStatus: "Subscribing to content topic..."
+    });
     subscription = await node.filter.createSubscription();
 
     await subscription.subscribe(decoder, (message) => {
       try {
+        
         const { timestamp, nick, text } = ProtoChatMessage.decode(
           message.payload
-        );
+        ) as unknown as IChatMessage;
 
         let proofStatus = "no proof";
         if (message.rateLimitProof) {
           console.log("Proof received: ", message.rateLimitProof);
 
           try {
+            if (!rln.contract) {
+              throw new Error("RLN contract not initialized");
+            }
+
             console.time("Proof verification took:");
             const res = message.verify(rln.contract.roots());
             console.timeEnd("Proof verification took:");
@@ -85,7 +109,10 @@ export async function initWaku({ rln, onStatusChange }) {
       }
     });
 
-    onStatusChange("Waku initialized", "success");
+    onStatusChange({
+      newStatus: "Waku initialized",
+      className: "success"
+    });
   };
 
   return {
