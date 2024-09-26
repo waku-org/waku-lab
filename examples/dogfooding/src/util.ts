8962b975ee789719069612361645900bcba44a73
Sasha <118575614+weboko@users.noreply.github.com>
2024-09-26 21:24:41 +0200

    feat: track discovery and improve websocket tracking (#91)

--- examples/dogfooding/src/util.ts
+++ examples/dogfooding/src/util.ts
@@ -1,5 +1,6 @@
 import { peerIdFromString } from "@libp2p/peer-id";
-import type { Waku } from "@waku/interfaces";
+import { Peer } from "@libp2p/interface";
+import type { LightNode } from "@waku/sdk";
 
 export const generateRandomNumber = (): number => {
     return Math.floor(Math.random() * 1000000);
@@ -17,17 +18,23 @@ export const sha256 = async (number: number | string ): Promise<string> => {
 const DEFAULT_EXTRA_DATA = { sdk: "0.0.28" };
 export const DEFAULT_EXTRA_DATA_STR = JSON.stringify(DEFAULT_EXTRA_DATA);
 
-export const buildExtraData = async (node: Waku, peerId: string): Promise<string> => {
-  const peer = await node.libp2p.peerStore.get(peerIdFromString(peerId));
-  const hasWebsockes = peer
+export const buildExtraData = async (node: LightNode, peerId: string): Promise<string> => {
+  let extraData = { ...DEFAULT_EXTRA_DATA };
+  const peer: Peer = await node.libp2p.peerStore.get(peerIdFromString(peerId));
+
+  if (!peer || peerId === node.libp2p.peerId.toString()) {
+    return JSON.stringify(extraData);
+  }
+
+  const websocket = peer
     .addresses
     .map(addr => addr.multiaddr.toString())
     .some(addr => addr.includes("ws") || addr.includes("wss"));
 
   return JSON.stringify({
-    ...DEFAULT_EXTRA_DATA,
+    ...extraData,
     peerId,
-    hasWebsockes,
+    websocket,
     enabledProtocols: peer.protocols,
   });
 };
