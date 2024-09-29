485ae306db136409fbb72bb59b49fd1fc75a7afa
Sasha <oleksandr@status.im>
2024-09-30 00:17:51 +0200

    rever telemetry

--- examples/dogfooding/src/telemetry_client.ts
+++ examples/dogfooding/src/telemetry_client.ts
@@ -57,6 +57,26 @@ export class TelemetryClient {
     }
   }
   private async send<T extends TelemetryMessage>(messages: T[]) {
-    return true;
+    const telemetryRequests = messages.map((message) => ({
+      id: ++this.requestId,
+      telemetryType: message.type.toString(),
+      telemetryData: message
+    }));
+
+    try {
+      const res = await fetch(this.url, {
+        method: "POST",
+        body: JSON.stringify(telemetryRequests),
+      });
+      if (res.status !== 201) {
+        console.error("Error sending messages to telemetry service: ", res.status, res.statusText, res.json);
+        return false
+      }
+      return true;
+    } catch (e) {
+      console.error("Error sending messages to telemetry service: ", e);
+      console.error("Failed trying to send the following messages: ", telemetryRequests);
+      return false;
+    }
   }
 }
