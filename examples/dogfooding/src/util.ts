53fdd481b071bd4519e0728109808433f35f8728
Sasha <oleksandr@status.im>
2024-10-01 14:15:05 +0200

    chore: use rc in dogfooding app

--- examples/dogfooding/src/util.ts
+++ examples/dogfooding/src/util.ts
@@ -14,7 +14,7 @@ export const sha256 = async (number: number | string ): Promise<string> => {
       .join("");
   };
 
-const DEFAULT_EXTRA_DATA = { sdk: "0.0.28" };
+const DEFAULT_EXTRA_DATA = { sdk: "0.0.29" };
 export const DEFAULT_EXTRA_DATA_STR = JSON.stringify(DEFAULT_EXTRA_DATA);
 
 export const buildExtraData = async (node: LightNode, peerId: string): Promise<string> => {
