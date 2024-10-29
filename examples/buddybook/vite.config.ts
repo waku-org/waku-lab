97be344b0198d331efec9efe284a40f4f580d49f
Danish Arora <danisharora099@gmail.com>
2024-10-29 16:20:46 +0530

    chore(buddybook): fix hosting

--- examples/buddybook/vite.config.ts
+++ examples/buddybook/vite.config.ts
@@ -9,4 +9,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 })
