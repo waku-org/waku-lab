1413b0a9f6f56ddfe00aed5e107d4ab876076540
Danish Arora <danisharora099@gmail.com>
2024-10-29 16:59:34 +0530

    chore(buddybook): fix base url

--- examples/buddybook/vite.config.ts
+++ examples/buddybook/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: './',
+  base: '/buddybook/',
   build: {
     assetsDir: 'assets',
     manifest: true,
