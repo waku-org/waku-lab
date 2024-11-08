b5f4b16b58980d9a06440c52d52b07e335171966
Danish Arora <danisharora099@gmail.com>
2024-11-08 10:40:26 +0700

    chore: remove base path

--- examples/buddybook/vite.config.ts
+++ examples/buddybook/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
-    base: '/buddybook/',
+    base: '/',
     plugins: [
       react(),
       {
