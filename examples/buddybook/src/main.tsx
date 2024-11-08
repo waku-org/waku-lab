2276135df5424e721d763ea3616e7d5feadc6972
Danish Arora <danisharora099@gmail.com>
2024-11-08 14:18:17 +0700

    chore: improvements

--- examples/buddybook/src/main.tsx
+++ examples/buddybook/src/main.tsx
@@ -27,7 +27,7 @@ createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <ConnectKitProvider>
           <LightNodeProvider options={WAKU_NODE_OPTIONS}>
-            <Router basename={import.meta.env.BASE_URL}>
+            <Router basename={'/'}>
               <App />
             </Router>
           </LightNodeProvider>
